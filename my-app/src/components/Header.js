12c8830e5f88559a87093a2559a653874fad831e
KamilaKisiel <kisiel.kamila@gmail.com>
2018-02-09 12:46:43 +0100

    react-app initial settings

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default () => {
+    let style = {
+        textAlign: 'center'
+    };
+    return <React.Fragment>
+        <header style={style}>'Tabela Graczy'</header>
+    </React.Fragment>;}
