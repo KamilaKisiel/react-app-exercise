5f633a1bac502f464cb135be68a8b0f0903f11b9
KamilaKisiel <kisiel.kamila@gmail.com>
2018-02-22 22:50:35 +0100

    set-up main app files

---
+++ my-app-task13/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Contacts from "./components/Contacts";
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Contacts/>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App
