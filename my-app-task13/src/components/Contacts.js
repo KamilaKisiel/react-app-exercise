ee911ca3bf0f515fa73b8d702721a3baa21e76f6
KamilaKisiel <kisiel.kamila@gmail.com>
2018-02-23 18:31:54 +0100

    add contacts class and addition formula

---
+++ my-app-task13/src/components/Contacts.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
+
+class Contacts extends React.Component {
+    state = {
+        contacts: [
+            {
+                id: '1',
+                name: 'Zenon Yeahbunny',
+                phone: 888888888,
+                email: 'example@mail.com',
+                category: 'family'
+            },
+            {
+                id: '2',
+                name: 'Bożena Walec',
+                phone: 333333333,
+                email: 'example@mail.com',
+                category: 'friend, job'
+            },
+            {
+                id: '3',
+                name: 'Adam Miauczyński',
+                phone: 444444444,
+                email: 'example@mail.com',
+                category: 'job'
+            }
+        ],
+    };
+
+    addContact = ({contactName, contactPhone, contactEmail, contactCategory}) => {
+        return this.setState({
+            contacts: this.state.contacts.concat({
+                    id: Date.now().toString(32),
+                    name: contactName,
+                    phone: contactPhone,
+                    email: contactEmail,
+                    category: contactCategory ? contactCategory : 'none'
+                }
+            )
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <AddContact addContact={this.addContact}/>
+                <ContactList contacts={this.state.contacts}/>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Contacts;
